f88eaab172970a5be12f65dadfeb624ef5ef2d72
Ubuntu <ubuntu@vm1.asia-south1-c.c.regal-mediator-342706.internal>
2022-05-05 08:40:26 +0000

    Crypto generated for 4 orgs

--- create-crypto-for-all.sh
+++ create-crypto-for-all.sh
@@ -28,3 +28,22 @@ echo "starting ca Org4"
 docker-compose up -d
 echo "creating crypto config for Org4"
 ./create-certificate-with-ca.sh
+
+echo "changing directory to FabricMultiHostDeployment"
+cd /home/ubuntu/FabricMultiHostDeployment
+echo "committing code"
+git add .
+git commit -m "Crypto generated for 4 orgs"
+echo "git pushing the code"
+git push
+
+echo "view running containers"
+docker ps -a 
+echo "view stop containers"
+docker stop $(docker ps -aq)
+echo "view remove containers"
+docker rm $(docker ps -aq)
+
+echo "view running containers"
+docker ps -a 
+
